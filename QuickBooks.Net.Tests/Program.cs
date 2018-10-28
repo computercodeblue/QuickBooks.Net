a65742ae073f60c60754a2f9e7ebb82caf36a43a
Chris Burke <chris@computercodeblue.com>
2018-10-28 01:54:10 -0500

    Fixed extra semi-colon in readme.md

--- QuickBooks.Net.Tests/Program.cs
+++ QuickBooks.Net.Tests/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 
-using QuickBooks.Net;
-
 namespace QuickBooks.Net.Tests
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            QuickBooksClient qb = new QuickBooksClient
+            {
+                ConsumerKey = "{consumerKey}",
+                ConsumerSecret = "{consumerSecret}",
+                AccessToken = "{access token}",
+                AccessTokenSecret = "{access token secret}",
+                CallbackUrl = "",
+                RealmId = "{realmId}",
+            };
+
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
